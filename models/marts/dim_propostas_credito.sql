23d1bdf86b67afc9a9c626f7bca6ad92b19f5d49
Mafra Michele <michele.mafra2@gmail.com>
2024-07-08 23:58:28 +0000

    commit ajustes na dim_transacoes, dim_propostas_credito e criei fc_gastos_por_tipos

--- models/marts/dim_propostas_credito.sql
+++ models/marts/dim_propostas_credito.sql
@@ -4,5 +4,25 @@ with
         from {{ ref('stg_banv__propostas_credito') }}
     )
 
-select * 
-from stg_propostas_credito
+    , stg_clientes as (
+        select *
+        from {{ ref('stg_banv__clientes') }}
+    )
+    
+    , joined_tabelas as (
+        select
+            stg.clientes.cod_cliente
+            , stg.clientes.nome_cliente
+            , stg_proposta_credito.cod_cliente
+            , stg_proposta_credito.valor_proposta
+            , stg_proposta_credito.valor_financiamento
+            , stg_proposta_credito.valor_entrada
+            , stg_proposta_credito.quantidade_parcelas
+            , stg_proposta_credito.carencia
+            , stg_proposta_credito.status_proposta
+        from stg_proposta_credito
+            left join stg_clientes on stg_proposta_credito.cod_cliente = stg.clientes.cod_cliente
+    )
+       
+select *
+from joined_tabelas
--- models/marts/dim_transacoes.sql
+++ models/marts/dim_transacoes.sql
@@ -9,22 +9,43 @@ with
         from {{ ref('stg_banv__contas') }}
     )
     
+    , stg_clientes as (
+        select *
+        from {{ ref('stg_banv__clientes') }}
+    )
+
     , joined_tabelas as (
         select
-        stg_transacoes.nome_transacao
+        stg_clientes.nome_cliente
+        , stg_clientes.cod_cliente 
         , stg_transacoes.data_transacao 
         , stg_transacoes.data_transacao
         , stg_transacoes.valor_transacao
         , stg_contas.tipo_conta
         , stg_contas.saldo_total
         , stg_contas.saldo_disponivel
-    from stg_transacoes
+        from stg_transacoes
         left join stg_contas on 
         stg_transacoes.num_conta = stg_contas.num_conta
+        left join stg_clientes on
+        stg_contas.cod_cliente = stg_clientes.cod_cliente
+        
+    )
+    
+    , agregar_tabelas as (
+        select
+            nome_cliente
+            , cod_cliente
+            , count(*) as numero_transacoes
+            , sum(valor_transacao) as total_gasto
+            , max(saldo_total) as saldo_total
+            , max(saldo_disponivel) as saldo_disponivel
+        from joined_tabelas
+        group by cod_cliente, nome_cliente 
     )
 
-
+    
 select * 
-from stg_transacoes
+from agregar_tabelas
 
     
